5d088ca57189988bda9ba1320f15b8d40c8e668a
yassineaouadi <yassine.aouadi@hotmail.fr>
2016-04-10 21:21:47 +0000

    insertion des données à partir du fichier yaml

--- insertion.sh
+++ insertion.sh
@@ -1,25 +1,36 @@
-
-#!/bin/sh
-
 echo "update"
 sudo apt-get update -y > /dev/null 2>&1
 echo "upgrade"
 sudo apt-get upgrade -y  > /dev/null 2>&1
-echo "installation de redis-server"
+echo " installation de redis server"
 sudo apt-get -y install -y redis-server > /dev/null 2>&1
-echo "Etat:"
+echo " status du service redis-server"
 sudo service redis-server status
-echo "lancer redis-server"
+echo "lancement du serveur "
 sudo service redis-server restart
-echo "fin du deploiement"
-echo "importation d'une librairie redis-bash-cli"
-git clone https://github.com/caquino/redis-bash.git
+
+echo "extraction des données à partir du fichier yaml"
+tab=( $( awk -F ":" '{print $1}' exemple.yml) )
+tab2=( $( awk -F ":" '{print $2}' exemple.yml) )
+chaine=""
+i=0
+n=${#tab[@]}
+while [ "$i" -lt "$(($n -1))" ]
+do
+chaine="$chaine ${tab[$i]} : ${tab2[$i]},"
+((i++))
+done
+chaine="$chaine ${tab[$i]} : ${tab2[$i]}"
+#echo "$chaine"
+echo "insertion des données dans la base "
+#echo "SET profiles:BrandonAmos  \"first : brandon, last: amos, phone:  ~(540)~947~1238, email: bamos@cs.cmu.edu, pdf: /data/cv.pdf ,src :  www.github.com/bamos/cv , url : bamos.github.io , github : bamos , twitter :brandonamos , linkedin :bdamos\"" | redis-cli
+echo "SET Profiles:Brandon \"$chaine\"" | redis-cli
+
+echo "affichage des données du fichier yaml dans la bse de donnée  redis"
+
+echo "GET Profiles:Brandon" | redis-cli
 
 
-source redis-bash/redis-bash-lib #inclure la librairie
-exec 6<>/dev/tcp/localhost/6379 # ouvrir la connexion
-echo "insertions des donnés  dans la base de données redis"
-redis-client 6 SET profiles:BrandonAmos  "first : brandon, last: amos, phone:  ~(540)~947~1238, email: bamos@cs.cmu.edu, pdf: /data/cv.pdf ,src :  www.github.com/bamos/cv , url : bamos.github.io , github : bamos , twitter :brandonamos , linkedin :bdamos"
-redis-client 6 GET profiles:BrandonAmos
-exec 6>&- 
-                  
+
+
+
